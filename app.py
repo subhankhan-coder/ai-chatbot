0687c1f76be986622c7e34d54eeb9851718d2bba
subhankhan-coder <subhankhan28@outlook.com>
2025-03-21 08:53:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+import os
+
+app = Flask(__name__)
+
+GEMINI_API_KEY = "AIzaSyC50LcX6xZPO1k__ZRwAOr0CSrGPwDrfnM"
+genai.configure(api_key=GEMINI_API_KEY)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        user_message = request.json.get("message")
+
+        model = genai.GenerativeModel("gemini-2.0-flash")
+
+        response = model.generate_content(user_message)
+        #ai_response = response.text  
+
+         # Remove markdown formatting
+        clean_response = response.text.replace('**', '')  # Remove bold markers
+        clean_response = clean_response.replace('*', '')   # Remove italics markers
+        clean_response = clean_response.replace('`', '')   # Remove code markers
+
+        return jsonify({"response": clean_response})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
